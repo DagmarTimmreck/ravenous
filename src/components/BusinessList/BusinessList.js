dd57c56a32e85108a15ab1a37eb017953c84f3f3
DagmarTimmreck <DagmarTimmreck@users.noreply.github.com>
2017-09-07 11:29:37 +0200

    completed basic version

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -7,12 +7,8 @@ class BusinessList extends React.Component {
   render() {
     return (
       <div className="BusinessList">
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
+        {this.props.businesses.map(function (business) {
+          return <Business key={business.id} business={business} />;})}
       </div>
     );
   }
